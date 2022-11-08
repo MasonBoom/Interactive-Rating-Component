76cf99b66ee7d0ae69251abd2cae261f920360d6
MasonBoom <masonboom@gmail.com>
2022-11-08 02:22:05 -0500

    start of project

---
+++ src/app.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Main = styled.div`
+    background: radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181E27 100%);
+    border-radius: 30px;
+    width: 400px;
+    height: 400px;
+    margin: 30vh auto;
+`;
+
+export const StarFrame = styled.div`
+    background: #262E38;
+    border-radius: 50%;
+    width: 48px;
+    height: 48px;
+`;
